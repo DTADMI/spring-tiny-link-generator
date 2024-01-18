e9732dc8353bf6141e5744bc0b062da72d7c5b64
TADMI Darryl <tadmidarryl@gmail.com>
2024-01-18 09:56:10 -0500

    * Clean up

--- src/main/java/ca/dtadmi/tinylink/service/MarshallService.java
+++ src/main/java/ca/dtadmi/tinylink/service/MarshallService.java
@@ -32,32 +32,10 @@ public class MarshallService {
             return urlPairs;
         }
         List<UrlPair> resultsData = new ArrayList<>(urlPairs);
-
+        logger.debug("Getting creation date sorted urlPairs {} most recent elements", maxNumberHistoryResults);
         return resultsData.stream()
                 .sorted(Comparator.comparing((UrlPair urlPair) -> new Date(urlPair.getCreationDate()).getTime()).reversed())
                 .limit(maxNumberHistoryResults).toList();
-    }
-
-    public static List<UrlPair> paginateResults(String page, String limit, List<UrlPair> urlPairs) {
-        if(urlPairs.isEmpty()) {
-            return urlPairs;
-        }
-        List<UrlPair> resultsData = new ArrayList<>(urlPairs);
-        int numberTasks = resultsData.size();
-
-        logger.debug("Getting sliced urlPairs with {} elements starting at page {}", limit, page);
-        // To optimize the display, 4 links are too much,
-        // thus limit cannot exceed 3, and page cannot exceed the number of pages
-        // Both cannot be less than 1 since pages start at 1, and we cannot take 0 elements
-        int perPage = Math.min(Integer.parseInt(limit), 3);
-        int pageCount = Math.ceilDivExact(numberTasks, perPage);
-        int limitInt = Math.max(1, Math.min(perPage, numberTasks));
-        int pageInt = Math.max(1, Math.min(Integer.parseInt(page), pageCount));
-        int startIndex = (pageInt - 1) * limitInt;
-        int endIndex = (pageInt) * limitInt;
-        logger.debug("Getting sliced urlPairs from {} to {} excluded", startIndex, endIndex);
-
-        return resultsData.subList(startIndex, endIndex);
     }
 
 }
